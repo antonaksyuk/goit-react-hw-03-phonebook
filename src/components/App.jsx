8ae32ce863aef4eeca5ecec9d5151f8bf1fa8ac0
antonaksyuk <87242472+antonaksyuk@users.noreply.github.com>
2023-06-26 17:25:17 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,10 @@
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      state = {
+  contacts: [],
+  name: ''
+}
     >
       React homework template
     </div>
